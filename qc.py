bb634507544bd545738d73aece5aad4e814b527e
Kristy Horan <kristyhoran15@gmail.com>
2015-10-24 20:11:45 +1100

    with _id set to uid

--- qc.py
+++ qc.py
@@ -1,12 +1,17 @@
 import sys
 import os
 import re
+from pymongo import MongoClient
+
 
 def main():
+	
+	db = connecttomongo()
 	#enter the directory where the files are stored
 	working_dir = sys.argv[1]
 	all_files = os.listdir(working_dir)
 	#open a file in the dir	
+	a_dict = {}
 	for sf in all_files:
 		if sf.endswith('-0.txt'):
 			of = open(working_dir+'/'+sf, 'r')
@@ -17,6 +22,7 @@ def main():
 			uid = re.sub("qc","sdat", re.split("-", uid)[0])
 			#add uid to dict
 			a_dict['uid']=uid
+
 			#select the area of txt to grab
 			y = [m.start() for m in re.finditer('>>END_MODULE', x)] # search x for >>END_MODULE assign to y - this returnsa list of indices whaich can be used to select data
 			text = x[y[0]:y[1]] #select the first block of >>END_MODULES
@@ -29,10 +35,22 @@ def main():
 				base_list.append(split_row[0])
 				qc_list.append(split_row[1])
 
-	   
+	   		a_dict['_id'] = uid
 			a_dict['quality'] = qc_list
 			a_dict['base'] = base_list
-			# print(a_dict)
+			#print(a_dict)
+			insertrecord(db, a_dict)
+
+def insertrecord(db, r):
+	db.insert(r)
+
+
+def connecttomongo():
+    #connection = MongoClient("mongodb://localhost:27017")
+    connection = MongoClient("mongodb://146.118.98.44:27017")
+    db_test = connection.healthhack.test_qc
+    return db_test
+
 				
 if __name__ == '__main__':
 	main()
